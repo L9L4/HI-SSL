49d4587934c3df28e2c0c373a82cc6744b6975f2
L9L4 <lorenzo.lastilla@uniroma1.it>
2021-05-28 16:17:21 +0200

    Small modifications SimCLR

--- csl/simclr/run_simclr.sh
+++ csl/simclr/run_simclr.sh
@@ -2,6 +2,4 @@ export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=8888
 export WORLD_SIZE=1
 pip install -r requirements.txt
-# python main_simclr.py -dir ./ -td ../Subsets_seed_34/train -vd ../Subsets_seed_34/val -c simclr_0 -g 1
-# python main_simclr.py -dir ./ -td ../Subsets_seed_34/train -vd ../Subsets_seed_34/val -c simclr_1 -g 1
 python main_simclr.py -dir ./ -td ../Subsets_seed_34/train -vd ../Subsets_seed_34/val -c simclr_2 -g 4
